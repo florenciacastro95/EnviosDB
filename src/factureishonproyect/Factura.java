55620ad6dd0a2b7836574a5e9e45f0edccaca68a
Florencia <fmcastro95@gmail.com>
2024-03-12 18:14:46 -0300

    Creé la clase main

---
+++ src/factureishonproyect/Factura.java
@@ -0,0 +1,25 @@
+
+package factureishonproyect;
+
+
+public class Factura {
+    char tipo;
+    double monto;
+
+    public char getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+    
+}
