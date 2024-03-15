b5c31a00a1ec3d9566318262f557d0e53e2eb9ee
tadeowoto <tadeo.woto@gmail.com>
2024-03-15 10:46:50 -0300

    adding a method to calculate the price of products

--- src/factureishonproyect/Productos.java
+++ src/factureishonproyect/Productos.java
@@ -12,6 +12,7 @@ public class Productos {
     private int codigo;
     private boolean vencido;
     private String nombreProducto;
+    private int precio;
 
     public Productos(int codigo, boolean vencido, String nombreProducto) {
         this.codigo = codigo;
@@ -47,6 +48,22 @@ public class Productos {
     @Override
     public String toString() {
         return "Productos{" + "codigo=" + codigo + ", vencido=" + vencido + ", nombreProducto=" + nombreProducto + '}';
+    }
+    
+    public void CalcularPrecio (){
+        // cree le metodo
+        int suma=0;
+        //estoy practicando 
+        if (nombreProducto == "agua") {
+            suma += precio*0.50;
+        }else{
+            System.out.println("El sistema no tiene forma de calcular ese tipo de descuento ya que no lo tiene");
+            System.out.println("No se que estoy haciendo xd");
+            System.out.println("Holaaaaaa, estan viendo el tadeo del pasado hacer cambios en su repositorio");
+            System.out.println("son las 10 y 45 de la mañana, esto es como viajar al pasado");
+        }
+        
+        
     }
     
     
